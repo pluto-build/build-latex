385008b684c57fd6334a3113ed1d5d3665fe2e30
Sebastian Erdweg <seba.github@gmail.com>
2015-04-05 08:55:16 +0200

    convert to maven

--- java-build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-java -cp cleardep.jar:bin-java Build $@
--- shake-build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-BIN=bin-shake
-mkdir -p $BIN
-ghc --make src-shake/Build.hs -rtsopts -with-rtsopts=-I0 -outputdir=$BIN -o $BIN/build && $BIN/build "$@"
